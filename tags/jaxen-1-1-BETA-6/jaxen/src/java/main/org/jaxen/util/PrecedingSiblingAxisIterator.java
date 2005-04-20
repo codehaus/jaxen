cbb174df786ced06847ed704a58d9c9d2033d946
nobody <nobody@43379f7c-b030-0410-81db-e0b70742847c>
2005-04-20 22:46:49 +0000

    This commit was manufactured by cvs2svn to create tag 'jaxen-1-1-BETA-6'.

---
+++ tags/jaxen-1-1-BETA-6/jaxen/src/java/main/org/jaxen/util/PrecedingSiblingAxisIterator.java
@@ -0,0 +1,152 @@
+/*
+ * $Header$
+ * $Revision$
+ * $Date$
+ *
+ * ====================================================================
+ *
+ * Copyright (C) 2000-2002 bob mcwhirter & James Strachan.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions, and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions, and the disclaimer that follows 
+ *    these conditions in the documentation and/or other materials 
+ *    provided with the distribution.
+ *
+ * 3. The name "Jaxen" must not be used to endorse or promote products
+ *    derived from this software without prior written permission.  For
+ *    written permission, please contact license@jaxen.org.
+ * 
+ * 4. Products derived from this software may not be called "Jaxen", nor
+ *    may "Jaxen" appear in their name, without prior written permission
+ *    from the Jaxen Project Management (pm@jaxen.org).
+ * 
+ * In addition, we request (but do not require) that you include in the 
+ * end-user documentation provided with the redistribution and/or in the 
+ * software itself an acknowledgement equivalent to the following:
+ *     "This product includes software developed by the
+ *      Jaxen Project (http://www.jaxen.org/)."
+ * Alternatively, the acknowledgment may be graphical using the logos 
+ * available at http://www.jaxen.org/
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE Jaxen AUTHORS OR THE PROJECT
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * ====================================================================
+ * This software consists of voluntary contributions made by many 
+ * individuals on behalf of the Jaxen Project and was originally 
+ * created by bob mcwhirter <bob@werken.com> and 
+ * James Strachan <jstrachan@apache.org>.  For more information on the 
+ * Jaxen Project, please see <http://www.jaxen.org/>.
+ * 
+ * $Id$
+ */
+
+
+
+package org.jaxen.util;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
+import org.jaxen.JaxenConstants;
+import org.jaxen.Navigator;
+import org.jaxen.UnsupportedAxisException;
+
+public class PrecedingSiblingAxisIterator implements Iterator
+{
+    private Object    contextNode;
+    private Navigator navigator;
+
+    private Iterator  siblingIter;
+    private Object    nextObj;
+
+    public PrecedingSiblingAxisIterator(Object contextNode,
+                                        Navigator navigator) throws UnsupportedAxisException
+    {
+        this.contextNode = contextNode;
+        this.navigator   = navigator;
+
+        init();
+        if ( siblingIter.hasNext() )
+        {
+            this.nextObj = siblingIter.next();
+        }
+    }
+
+    private void init() throws UnsupportedAxisException
+    {
+        
+        Object parent = this.navigator.getParentNode( this.contextNode );
+
+        if ( parent != null )
+        {
+            Iterator childIter = this.navigator.getChildAxisIterator( parent );
+            LinkedList siblings = new LinkedList();
+            
+            while ( childIter.hasNext() )
+            {
+                Object eachChild = childIter.next();
+                if ( eachChild.equals(this.contextNode) )
+                {
+                    break;
+                }
+                siblings.addFirst( eachChild );
+            }
+            
+            this.siblingIter = siblings.iterator();
+            
+        }
+        else {
+            this.siblingIter = JaxenConstants.EMPTY_ITERATOR;
+        }
+        
+    }
+
+    public boolean hasNext()
+    {
+        return ( this.nextObj != null );
+    }
+
+    public Object next() throws NoSuchElementException
+    {
+        if ( ! hasNext() )
+        {
+            throw new NoSuchElementException();
+        }
+
+        Object obj = this.nextObj;
+        if ( siblingIter.hasNext() )
+        {
+            this.nextObj = siblingIter.next();
+        }
+        else {
+            this.nextObj = null;
+        }
+        return obj;
+    }
+
+    public void remove() throws UnsupportedOperationException
+    {
+        throw new UnsupportedOperationException();
+    }
+
+}
